1c88f1e95441bbd166b08d5fb0e8e59d673021aa
JJ <jonathan.b4lzan0@gmail.com>
2023-11-24 08:09:16 +0100

    Added bash prerequisites file

---
+++ WeatherService/prerequisites.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Python
+# No specific installation is needed as Python comes pre-installed in many operating systems. If Python is not installed, you can use the package manager of your operating system to install it.
+
+# Install Flask
+python -m pip install flask
+
+# Install the Requests library
+python -m pip install requests
